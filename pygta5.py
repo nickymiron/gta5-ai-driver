acf326650795c4bf6a8c57db204008083ceeea4a
Nick Mironenko <nickmironenko@gmail.com>
2017-05-21 12:20:36 +0300

    Defining region of interest

--- pygta5.py
+++ pygta5.py
@@ -2,36 +2,35 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+from directkeys import ReleaseKey, PressKey, W, A, S, D
 import pyautogui
-from directkeys import PressKey, W, A, S, D
 
 
-def process_img(image):
-    original_image = image
-    # convert to gray
-    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # edge detection
+def roi(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    vertices = np.array([[10,500],[10,300], [300,200], [500,200], [800,300], [800,500]], np.int32)
+    processed_img = roi(processed_img, [vertices])
     return processed_img
 
 
 def main():
-    for i in list(range(4))[::-1]:
-        print(i + 1)
-        time.sleep(1)
-
     last_time = time.time()
-    while True:
-        PressKey(W)
-        screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-        # print('Frame took {} seconds'.format(time.time()-last_time))
-        last_time = time.time()
+    while(True):
+        screen =  np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
         new_screen = process_img(screen)
+        print('Loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
         cv2.imshow('window', new_screen)
-        # cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
-
 
 main()
